40269c1a1ef6c2cfcccc0553c75d0f5eefd5d62e
KR-HS <jjc5989@naver.com>
2024-11-22 16:07:11 +0900

    4일차 수정

---
+++ Java(4일차)/src/Main.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int count=sc.nextInt();
+        /*
+         * 백준 알고리즘
+         * https://www.acmicpc.net/problem/2563
+         */
+        // int[][] position = new int[100][100];
+
+        // sc.nextLine();
+        
+        // while(count>0){
+        //     System.out.print("두 변 입력 : ");
+        //     String line = sc.nextLine();
+
+        //     if(line.isEmpty()){
+        //         break;
+        //     }
+            
+        //     String[] tokens=line.split(" ");
+        //     int x=Integer.parseInt(tokens[0]);
+        //     int y=Integer.parseInt(tokens[1]);
+            
+        //     for(int i=x;i<x+10;i++){
+        //         for(int j=y;j<y+10;j++){
+        //             position[i][j]=1;
+        //         }
+        //     }
+        //     count--;
+
+        // }
+        // int sum=0;
+        // for(int[] po:position){
+        //     for(int p:po){
+        //         sum+=p;
+        //     }
+        // }
+        // System.out.println(sum);
+        
+
+        /*
+         * 백준 알고리즘
+         * https://www.acmicpc.net/problem/2292
+         */
+        // if(count==1){
+        //     System.out.println(1);
+        //     return;
+        // }
+
+        // int level=1;
+        // int rooms=1;
+
+        // while(rooms<count){
+        //     rooms+=6*level;
+        //     level++;
+        // }
+
+        // System.out.println(level);
+        
+
+        /*
+         * 백준 알고리즘
+         * https://www.acmicpc.net/problem/2869
+         */
+        // int down=sc.nextInt();
+        // int height = sc.nextInt();
+        // int current_pos=0;
+        // int days=0;
+
+
+        // System.out.println((int)(Math.ceil(((height-down)*1.0/(count-down)))));
+        
+        
+        /*
+         * 백준 알고리즘
+         * https://www.acmicpc.net/problem/1193
+         */
+        if(count==1){
+            System.out.println("1/1");
+        }
+        else{
+            int num=0;
+            for(int i=1;i<10000;i++){
+                if((double)((i-1)*i)/2<count && count <=(i*(i+1)/2)){
+                    num=count-i*(i-1)/2;
+                    if(i%2==0){
+                        System.out.printf("%d/%d",num,i+1-num);
+                    }
+                    else{
+                        System.out.printf("%d/%d",i+1-num,num);
+                    }
+                    break;
+                }
+            }
+    
+        }
+
+    }
+
+    
+}
--- Java(4일차)/src/Study11.java
+++ Java(4일차)/src/Study11.java
@@ -1,26 +1,51 @@
 public class Study11 {
     public static void main(String[] args){
-        String[][] arr = new String[5][3];
+        Study11 st = new Study11();
+
+        String[][] arr=st.InputValue();
+        st.PrintValue(arr);
+        
+        //String[][] rotateArr=st.RotateValue(arr);
+        arr=st.RotateValue(arr);
+        //st.PrintValue(rotateArr);
+        st.PrintValue(arr);
+    }
+
+    public String[][] InputValue(){
+        String[][] arr = new String[10][10];
 
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[0].length;j++){
-                if(j%2==1){
-                    arr[i][j]=(i%2==0)?"X":"O";
+                if(j<=i){
+                    arr[i][j]="*";
                 }
                 else{
-                    arr[i][j]=(i%2==0)?"O":"X";
+                    arr[i][j]=" ";
                 }
             }
         }
-        
-        for(String[] ar:arr){
-            for(String a:ar){
+        return arr;
+    }
+
+    public void PrintValue(String[][] arr){
+        for(String[] ar: arr){
+            for(String a: ar){
                 System.out.print(a+" ");
             }
             System.out.println();
         }
+    }
 
+    public String[][] RotateValue(String[][] arr){
+        String[][] rotateArr=new String[10][10];
 
+        System.out.println("90도 회전");
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                rotateArr[j][arr.length-i-1]=arr[i][j];
+            }
+        }
 
+        return rotateArr;
     }
 }
